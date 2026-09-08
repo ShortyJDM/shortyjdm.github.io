8ad9775afc8151dda84887e911ea10e886115dd8
Jay Megill <jmegill7@unm.edu>
2026-09-08 13:45:19 -0600

    Added scale function

--- VertexShader.js
+++ VertexShader.js
@@ -5,11 +5,24 @@
   uniform float uTime; //time in sec
   out vec3 vColor;
 
+  vec3 scale(in vec3 position, in float scaleX, in float scaleY);
+
+  vec3 mPosition;
+
   void main() {
-    gl_Position = vec4(aPosition, 1.0);
+    mPosition = scale(aPosition, 1.0, 0.5);
+    gl_Position = vec4(mPosition, 1.0);
     if (mod(uTime,12.566) > 6.28318) {
     vColor = aColor * 2.0 * vec3(sin(uTime+(2.0*float(gl_VertexID))),sin(uTime+(2.0*float(gl_VertexID-1))),sin(uTime+(2.0*float(gl_VertexID-2))));
     } else {
     vColor = 5.0*vec3(sin(uTime/2.0),sin(uTime/2.0),sin(uTime/2.0)) * vec3(sin(uTime+(2.0*float(gl_VertexID))),sin(uTime+(2.0*float(gl_VertexID-1))),sin(uTime+(2.0*float(gl_VertexID-2))));
     }
+  }
+
+  vec3 scale(in vec3 position, in float scaleX, in float scaleY){
+    return mat3x3(
+      scaleX, 0, 0,
+      0, scaleY, 0,
+      0, 0,      1
+    ) * position;
   }
