8b088f44edee5186142167a9de7e3882513b37d6
Jay Megill <jmegill7@unm.edu>
2026-09-09 09:23:24 -0600

    Added 2D transformation functions to VertexShader

--- VertexShader.js
+++ VertexShader.js
@@ -39,10 +39,26 @@
     ) * position;
   }
 
-    vec3 translate(in vec3 position, in float tx, in float ty){
+  vec3 translate(in vec3 position, in float tx, in float ty){
     return mat3x3(
       1, 0, 0,
       0, 1, 0,
       tx, ty, 1
+    ) * position;
+  }
+
+  vec3 skew(in vec3 position, in float shx, in float shy){
+    return mat3x3(
+      1, shy, 0,
+      shx, 1, 0,
+      0, 0, 1
+    ) * position;
+  }
+
+  vec3 mirror(in vec3 position, in float angle){
+      return mat3x3(
+      cos(2.0*angle), sin(2.0*angle), 0,
+      sin(2.0*angle), -cos(2.0*angle), 0,
+      0, 0, 1
     ) * position;
   }
