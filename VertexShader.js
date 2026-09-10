1fcb59f49d77e81d5f49d95af9898659d866e7f8
Jay Megill <jmegill7@unm.edu>
2026-09-10 14:31:52 -0600

    cleaned up transformation. Translation works now. (All functions are pseudo 3D) Added identity function

--- VertexShader.js
+++ VertexShader.js
@@ -5,15 +5,18 @@
   uniform float uTime; //time in sec
   out vec3 vColor;
 
-  mat3x3 scale(in float sx, in float sy);
-  mat3x3 rotate(in float angle);
-  mat3x3 translate(in float tx, in float ty);
-  mat3x3 skew(in float shx, in float shy);
-  mat3x3 mirror(in float angle);
+  mat4x4 identity();
+  mat4x4 scale(in float sx, in float sy);
+  mat4x4 rotate(in float angle);
+  mat4x4 translate(in float tx, in float ty);
+  mat4x4 skew(in float shx, in float shy);
+  mat4x4 mirror(in float angle);
 
 
   void main() {
-    gl_Position = vec4(rotate(uTime+sin(uTime)+3.1415)*(scale(0.4,0.4)*aPosition+vec3(0.6,0.0,0.0)), 1.0);
+    mat4x4 tMat = identity();
+    tMat = tMat *scale(0.4,0.4) * translate(0.6,0.0)*rotate(uTime+sin(uTime)+3.1415);
+    gl_Position = tMat * vec4(aPosition, 1.0);
     if (mod(uTime,12.566) > 6.28318) {
     vColor = aColor * 2.0 * vec3(sin(uTime+(2.0*float(gl_VertexID))),sin(uTime+(2.0*float(gl_VertexID-1))),sin(uTime+(2.0*float(gl_VertexID-2))));
     } else {
@@ -22,43 +25,59 @@
   }
 
 
-  mat3x3 scale(in float sx, in float sy){
-    return mat3x3(
-      sx, 0, 0,
-      0, sy, 0,
-      0, 0, 1
+  mat4x4 identity(){
+    return mat4x4(
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
     );
   }
 
-  mat3x3 rotate(in float angle){
-      return mat3x3(
-      cos(angle), sin(angle), 0,
-      -sin(angle), cos(angle), 0,
-      0, 0, 1
+
+  mat4x4 scale(in float sx, in float sy){
+    return mat4x4(
+      sx, 0, 0, 0,
+      0, sy, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
     );
   }
 
-  mat3x3 translate(in float tx, in float ty){
-    return mat3x3(
-      1, 0, 0,
-      0, 1, 0,
-      tx, ty, 1
+  mat4x4 rotate(in float angle){
+      return mat4x4(
+      cos(angle), sin(angle), 0, 0,
+      -sin(angle), cos(angle), 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    );
+  }
+ 
+  //doesn't work?
+  mat4x4 translate(in float tx, in float ty){
+    return mat4x4(
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      tx, ty, 0, 1
     );
   }
 
-  mat3x3 skew(in float shx, in float shy){
-    return mat3x3(
-      1, shy, 0,
-      shx, 1, 0,
-      0, 0, 1
+  mat4x4 skew(in float shx, in float shy){
+    return mat4x4(
+      1, shy, 0, 0,
+      shx, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
     );
   }
 
-  mat3x3 mirror(in float angle){
-      return mat3x3(
-      cos(2.0*angle), sin(2.0*angle), 0,
-      sin(2.0*angle), -cos(2.0*angle), 0,
-      0, 0, 1
+  mat4x4 mirror(in float angle){
+      return mat4x4(
+      cos(2.0*angle), sin(2.0*angle), 0, 0,
+      sin(2.0*angle), -cos(2.0*angle), 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
     );
   }
   
