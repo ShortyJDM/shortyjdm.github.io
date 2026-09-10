53b6247322f0d4f2ebafcaa15ae5a9657028bb11
Jay Megill <jmegill7@unm.edu>
2026-09-10 13:05:24 -0600

    Fixed triangle not displaying. Added animation with rotate, scale, and placeholder transpose

--- VertexShader.js
+++ VertexShader.js
@@ -5,17 +5,15 @@
   uniform float uTime; //time in sec
   out vec3 vColor;
 
-  vec3 scale(in vec3 position, in float sx, in float sy);
-  vec3 rotate(in vec3 position, in float angle);
-  vec3 translate(in vec3 position, in float tx, in float ty);
-  vec3 skew(in vec3 position, in float shx, in float shy);
-  vec3 mirror(in vec3 position, in float angle);
+  mat3x3 scale(in float sx, in float sy);
+  mat3x3 rotate(in float angle);
+  mat3x3 translate(in float tx, in float ty);
+  mat3x3 skew(in float shx, in float shy);
+  mat3x3 mirror(in float angle);
 
-  vec3 mPosition;
 
   void main() {
-    mPosition = scale(aPosition, 1.0, 0.5);
-    gl_Position = vec4(mPosition, 1.0);
+    gl_Position = vec4(rotate(uTime+sin(uTime)+3.1415)*(scale(0.4,0.4)*aPosition+vec3(0.6,0.0,0.0)), 1.0);
     if (mod(uTime,12.566) > 6.28318) {
     vColor = aColor * 2.0 * vec3(sin(uTime+(2.0*float(gl_VertexID))),sin(uTime+(2.0*float(gl_VertexID-1))),sin(uTime+(2.0*float(gl_VertexID-2))));
     } else {
@@ -23,42 +21,44 @@
     }
   }
 
-  vec3 scale(in vec3 position, in float sx, in float sy){
+
+  mat3x3 scale(in float sx, in float sy){
     return mat3x3(
       sx, 0, 0,
       0, sy, 0,
       0, 0, 1
-    ) * position;
+    );
   }
 
-  vec3 rotate(in vec3 position, in float angle){
+  mat3x3 rotate(in float angle){
       return mat3x3(
       cos(angle), sin(angle), 0,
       -sin(angle), cos(angle), 0,
       0, 0, 1
-    ) * position;
+    );
   }
 
-  vec3 translate(in vec3 position, in float tx, in float ty){
+  mat3x3 translate(in float tx, in float ty){
     return mat3x3(
       1, 0, 0,
       0, 1, 0,
       tx, ty, 1
-    ) * position;
+    );
   }
 
-  vec3 skew(in vec3 position, in float shx, in float shy){
+  mat3x3 skew(in float shx, in float shy){
     return mat3x3(
       1, shy, 0,
       shx, 1, 0,
       0, 0, 1
-    ) * position;
+    );
   }
 
-  vec3 mirror(in vec3 position, in float angle){
+  mat3x3 mirror(in float angle){
       return mat3x3(
       cos(2.0*angle), sin(2.0*angle), 0,
       sin(2.0*angle), -cos(2.0*angle), 0,
       0, 0, 1
-    ) * position;
+    );
   }
+  
